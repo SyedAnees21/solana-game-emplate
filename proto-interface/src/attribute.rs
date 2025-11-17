06e86e164c8d9948de3ac0c616297dc4ab99a82d
Anees <syedanees816@gmail.com>
2025-11-18 02:38:17 +0500

    feat: web3 protocol types

--- proto-interface/src/attribute.rs
+++ proto-interface/src/attribute.rs
@@ -1,9 +1,12 @@
+use arrayvec::ArrayString;
+
 use crate::{
+    bytes,
     external::{
         attributes::{attribute::Value as ExtValue, Attribute as ExtAttribute, Vec3, Vec4},
         messages::Entity,
     },
-    AttributeId, EntityId, Timestamp,
+    from_bytes, web3, AttributeId, EntityId, Timestamp, ValuePayload, ValueTag, MAX_STRING_LENGTH,
 };
 
 #[derive(Clone, Debug, PartialEq, Default)]
@@ -13,12 +16,79 @@ pub enum Value {
     Bool(bool),
     Vec3(f64, f64, f64),
     Vec4(f64, f64, f64, f64),
-    String(String),
+    String(ArrayString<MAX_STRING_LENGTH>),
 }
 
 impl Value {
     pub fn is_none(&self) -> bool {
         matches!(self, Value::None)
+    }
+
+    pub fn to_payload(&self) -> (ValueTag, ValuePayload) {
+        let mut payload = ValuePayload::default();
+
+        fn fill_payload(payload: &mut ValuePayload, bytes: impl AsRef<[u8]>, delimited: bool) {
+            let bytes = bytes.as_ref();
+            let mut til = bytes.len();
+            let mut from = 0;
+
+            if delimited {
+                from = 1;
+                til += 1;
+                payload.as_mut()[0] = til as u8;
+            }
+            payload.as_mut()[from..til].copy_from_slice(&bytes[..]);
+        }
+
+        match self {
+            Value::None => (web3::tags::NONE.into(), payload),
+            Value::Bool(b) => {
+                if *b {
+                    payload.as_mut()[0] = 1
+                };
+
+                (web3::tags::BOOL.into(), payload)
+            }
+            Value::Vec3(x, y, z) => {
+                let bytes = bytes!(x, y, z);
+                fill_payload(&mut payload, bytes, false);
+
+                (web3::tags::VEC3.into(), payload)
+            }
+            Value::Vec4(x, y, z, w) => {
+                let bytes = bytes!(x, y, z, w);
+                fill_payload(&mut payload, bytes, false);
+
+                (web3::tags::VEC4.into(), payload)
+            }
+            Value::String(s) => {
+                let bytes = bytes!(s, String);
+                fill_payload(&mut payload, bytes, true);
+
+                (web3::tags::STRING.into(), payload)
+            }
+        }
+    }
+
+    pub fn from_payload(payload: ValuePayload, tag: ValueTag) -> Self {
+        let tag = *tag.as_ref();
+        match tag {
+            t if t == web3::tags::NONE => Value::None,
+            t if t == web3::tags::BOOL => Value::Bool(payload.as_ref()[0] == 1),
+            t if t == web3::tags::VEC3 => {
+                let v = from_bytes!(payload.as_ref(), 24, f64);
+                Value::Vec3(v[0], v[1], v[2])
+            }
+            t if t == web3::tags::VEC4 => {
+                let v = from_bytes!(payload.as_ref(), 32, f64);
+                Value::Vec4(v[0], v[1], v[2], v[3])
+            }
+            t if t == web3::tags::STRING => {
+                let v = from_bytes!(payload.as_ref(), String);
+                Value::String(ArrayString::from(&v).unwrap_or_default())
+            }
+            _ => unreachable!("Should not be reached for unknown tags"),
+        }
     }
 }
 
@@ -28,7 +98,7 @@ impl From<Value> for ExtValue {
             Value::Bool(b) => ExtValue::Bool(b),
             Value::Vec3(x, y, z) => ExtValue::Vec3(Vec3 { x, y, z }),
             Value::Vec4(x, y, z, w) => ExtValue::Vec4(Vec4 { x, y, z, w }),
-            Value::String(s) => ExtValue::String(s),
+            Value::String(s) => ExtValue::String(s.to_string()),
             _ => todo!("Need to handle the none case"),
         }
     }
@@ -40,7 +110,10 @@ impl Into<Value> for ExtValue {
             ExtValue::Bool(b) => Value::Bool(b),
             ExtValue::Vec3(v) => Value::Vec3(v.x, v.y, v.z),
             ExtValue::Vec4(v) => Value::Vec4(v.x, v.y, v.z, v.w),
-            ExtValue::String(s) => Value::String(s),
+
+            // TODO: Handle the proper conversion in case of string received
+            // larger than the allowed length instead of storing an empty value.
+            ExtValue::String(s) => Value::String(ArrayString::from(&s).unwrap_or_default()),
         }
     }
 }
@@ -61,5 +134,51 @@ impl From<(EntityId, Vec<ExtAttribute>)> for Entity {
             id: value.0,
             attrs: value.1,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn addition() {
+        println!("{}", 1 + 1);
+        println!("{}", "Dhanno")
+    }
+
+    #[test]
+    fn values_payload() {
+        let b = Value::Bool(true);
+        let v3 = Value::Vec3(1., 2., 3.);
+        let v4 = Value::Vec4(1., 2., 3., 4.);
+        let s = Value::String(ArrayString::from("ABCDefgh").unwrap());
+
+        let len = "ABCDefgh".as_bytes().len() + 1;
+
+        let (tag, payload) = b.to_payload();
+        assert_eq!((*tag.as_ref(), payload.as_ref()[0]), (web3::tags::BOOL, 1));
+
+        let (tag, payload) = v3.to_payload();
+        assert_eq!(
+            (*tag.as_ref(), payload.as_ref()[..24].into()),
+            (web3::tags::VEC3, bytes!(1_f64, 2_f64, 3_f64))
+        );
+
+        let (tag, payload) = v4.to_payload();
+        assert_eq!(
+            (*tag.as_ref(), payload.as_ref()[..32].into()),
+            (web3::tags::VEC4, bytes!(1_f64, 2_f64, 3_f64, 4_f64))
+        );
+
+        let (tag, payload) = s.to_payload();
+        assert_eq!(
+            (
+                *tag.as_ref(),
+                payload.as_ref()[1..len].into(),
+                payload.as_ref()[0]
+            ),
+            (web3::tags::STRING, bytes!("ABCDefgh", String), len as u8)
+        );
     }
 }
--- proto-interface/src/lib.rs
+++ proto-interface/src/lib.rs
@@ -1,19 +1,58 @@
-pub use external::messages::TickUpdate;
-pub use external::attributes::Attribute;
 pub use crate::attribute::Value;
+pub use external::attributes::Attribute;
+pub use external::messages::TickUpdate;
+pub use web3::*;
 
-pub mod external;
 pub mod attribute;
-pub mod outgoing;
+pub mod external;
+pub mod macros;
 pub mod messages;
+pub mod outgoing;
 pub mod updates;
+pub mod web3;
 
 pub type AttributeId = u32;
 pub type SessionId = u64;
 pub type Timestamp = u64;
 pub type EntityId = u32;
-pub type PlayerId = SessionId;
+pub type PlayerId = u64;
 
 pub const MAX_ATTRIBUTES_PER_COMPONENT: usize = 128;
-pub const MAX_STRING_LENGTH: usize = 64;
+pub const MAX_STRING_LENGTH: usize = 30;
+pub const WEB3_VALUE_PAYLOAD_BYTES: usize = 64;
 
+pub fn fill_payload<B>(payload: &mut ValuePayload, bytes: B)
+where
+    B: AsRef<[u8]>,
+{
+    let bytes = bytes.as_ref();
+    let til = bytes.len();
+
+    payload.as_mut()[..til].copy_from_slice(&bytes[..]);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{bytes, fill_payload, from_bytes, ValuePayload};
+
+    #[test]
+    fn payload_operations() {
+        let x: f64 = 1.;
+        let y: f64 = 2.;
+        let z: f64 = 3.;
+        let w: f64 = 4.;
+        let string = String::from("ABCDFEGH");
+        let mut payload: ValuePayload = ValuePayload::default();
+
+        let bytes = bytes!(string, String);
+        fill_payload(&mut payload, bytes);
+
+        assert_eq!(&payload.as_ref()[..bytes.len()], bytes);
+        assert_eq!(from_bytes!(&payload.as_ref()[..bytes.len()], String), string);
+
+        payload = ValuePayload::default();
+
+        fill_payload(&mut payload, bytes!(x, y, z, w));
+        assert_eq!(from_bytes!(payload.as_ref(), 32, f64), [x, y, z, w]);
+    }
+}
