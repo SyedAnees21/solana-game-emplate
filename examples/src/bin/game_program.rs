255c0708188b38cc41a50f022bc920a3c6508e2a
Anees <syedanees816@gmail.com>
2025-11-19 22:04:09 +0500

    feat: on chain game manager example

--- examples/src/bin/game_program.rs
+++ examples/src/bin/game_program.rs
@@ -3,18 +3,24 @@ use anchor_client::{
         commitment_config::CommitmentConfig,
         native_token::LAMPORTS_PER_SOL,
         signature::{Keypair, Signature},
-        signer::Signer,
+        signer::{EncodableKey, Signer},
     },
     Client, Cluster, Program,
 };
 use anchor_lang::{declare_program, prelude::system_program, InstructionData, ToAccountMetas};
 use anyhow;
-use proto_interface::{bytes, EntityId, PlayerId, PDA};
+use proto_interface::{bytes, EntityId, InlineString, PlayerId, PDA};
 use rand::rngs::OsRng;
 use rand::RngCore;
 use std::{ops::Deref, rc::Rc, thread::sleep, time::Duration};
 
-use crate::solana_game::client::accounts::{InitializeEntity, InitializeGame};
+use crate::solana_game::{
+    client::{
+        accounts::{InitializeEntity, InitializeGame, InitializePlayer},
+        args,
+    },
+    types::{self},
+};
 
 declare_program!(solana_game);
 
@@ -24,7 +30,8 @@ fn main() -> anyhow::Result<()> {
     let program_id = solana_game::ID;
     println!("Using program id: {}", program_id);
 
-    let payer = Rc::new(Keypair::new());
+    let provider_keypair = Keypair::read_from_file("./assets/provider_wallet.json").unwrap();
+    let payer = Rc::new(provider_keypair);
     let player = Rc::new(Keypair::new());
 
     let game_id = rng.next_u64();
@@ -43,10 +50,6 @@ fn main() -> anyhow::Result<()> {
         .rpc()
         .request_airdrop(&player.pubkey(), 5 * LAMPORTS_PER_SOL)?;
 
-    program
-        .rpc()
-        .request_airdrop(&payer.pubkey(), 5 * LAMPORTS_PER_SOL)?;
-
     sleep(Duration::from_millis(500));
 
     let entity_pda = PDA!(
@@ -58,26 +61,47 @@ fn main() -> anyhow::Result<()> {
     let player_pda = PDA!(["player".as_bytes(), bytes!(player_id)], program_id).0;
     let game_state_pda = PDA!(["game".as_bytes(), bytes!(game_id)], program_id).0;
 
+    let inst = InitializeGame {
+        game_state: game_state_pda,
+        game_owner: payer.pubkey(),
+        system_program: system_program::ID,
+    };
 
-    // let inst = InitializeGame {
-    //     game_state: game_state_pda,
+    let args = args::InitializeGame {
+        game_id,
+        game_name: InlineString::from("TPP Demo").into(),
+    };
+
+    send_transaction(&program, inst, args, &payer)?;
+
+    let inst = InitializePlayer {
+        player_state: player_pda,
+        game_state: game_state_pda,
+        game_owner: payer.pubkey(),
+        system_program: system_program::ID,
+    };
+
+    let args = args::InitializePlayer {
+        _game_id: game_id,
+        player_id,
+        player_name: InlineString::from("Gatsby").into(),
+    };
+
+    send_transaction(&program, inst, args, &payer)?;
+
+    // let inst = InitializeEntity {
+    //     entity: entity_pda,
     //     player: player.pubkey(),
     //     system_program: system_program::ID,
     // };
 
-    let inst = InitializeEntity {
-        entity: entity_pda,
-        player: player.pubkey(),
-        system_program: system_program::ID,
-    };
+    // let args = solana_game::client::args::InitializeEntity {
+    //     _player_id: player_id,
+    //     entity_id: entity_id,
+    // };
 
-    let args = solana_game::client::args::InitializeEntity {
-        _player_id: player_id,
-        entity_id: entity_id,
-    };
-
-    send_transaction(&program, inst, args, &player)?;
-    let entity_state = program.account::<solana_game::accounts::EntityState>(entity_pda);
+    // send_transaction(&program, inst, args, &player)?;
+    let entity_state = program.account::<solana_game::accounts::GameState>(game_state_pda);
 
     println!("{:?}", entity_state);
 
@@ -104,4 +128,13 @@ where
         .send()?;
 
     Ok(sig)
+}
+
+impl From<InlineString> for types::InlineString {
+    fn from(value: InlineString) -> Self {
+        Self {
+            len: value.get_len() as u8,
+            buffer: value.as_ref().to_owned(),
+        }
+    }
 }
